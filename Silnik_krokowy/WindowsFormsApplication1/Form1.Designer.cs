4276c9049f68778497aa6b93a10a5dff14b96b30
OskarS <oskar.szubert@gmail.com>
2018-01-02 21:07:35 +0100

    Silnik krokowy- projekt

---
+++ Silnik_krokowy/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lewoKat = new System.Windows.Forms.Button();
+            this.prawoKat = new System.Windows.Forms.Button();
+            this.kroki = new System.Windows.Forms.TextBox();
+            this.kat = new System.Windows.Forms.TextBox();
+            this.speed = new System.Windows.Forms.TextBox();
+            this.lewoKroki = new System.Windows.Forms.Button();
+            this.prawoKroki = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lewoKat
+            // 
+            this.lewoKat.Location = new System.Drawing.Point(200, 41);
+            this.lewoKat.Name = "lewoKat";
+            this.lewoKat.Size = new System.Drawing.Size(75, 23);
+            this.lewoKat.TabIndex = 0;
+            this.lewoKat.Text = "lewo s1";
+            this.lewoKat.UseVisualStyleBackColor = true;
+            this.lewoKat.Click += new System.EventHandler(this.lewoKat_Click);
+            // 
+            // prawoKat
+            // 
+            this.prawoKat.Location = new System.Drawing.Point(291, 41);
+            this.prawoKat.Name = "prawoKat";
+            this.prawoKat.Size = new System.Drawing.Size(75, 23);
+            this.prawoKat.TabIndex = 1;
+            this.prawoKat.Text = "prawo s1";
+            this.prawoKat.UseVisualStyleBackColor = true;
+            this.prawoKat.Click += new System.EventHandler(this.prawoKat_Click);
+            // 
+            // kroki
+            // 
+            this.kroki.Location = new System.Drawing.Point(68, 12);
+            this.kroki.Name = "kroki";
+            this.kroki.Size = new System.Drawing.Size(100, 20);
+            this.kroki.TabIndex = 2;
+            this.kroki.Text = "1";
+            // 
+            // kat
+            // 
+            this.kat.Location = new System.Drawing.Point(68, 38);
+            this.kat.Name = "kat";
+            this.kat.Size = new System.Drawing.Size(100, 20);
+            this.kat.TabIndex = 3;
+            this.kat.Text = "30";
+            // 
+            // speed
+            // 
+            this.speed.Location = new System.Drawing.Point(68, 64);
+            this.speed.Name = "speed";
+            this.speed.Size = new System.Drawing.Size(100, 20);
+            this.speed.TabIndex = 4;
+            this.speed.Text = "60";
+            // 
+            // lewoKroki
+            // 
+            this.lewoKroki.Location = new System.Drawing.Point(200, 12);
+            this.lewoKroki.Name = "lewoKroki";
+            this.lewoKroki.Size = new System.Drawing.Size(75, 23);
+            this.lewoKroki.TabIndex = 5;
+            this.lewoKroki.Text = "lewo s1";
+            this.lewoKroki.UseVisualStyleBackColor = true;
+            this.lewoKroki.Click += new System.EventHandler(this.lewoKroki_Click);
+            // 
+            // prawoKroki
+            // 
+            this.prawoKroki.Location = new System.Drawing.Point(291, 12);
+            this.prawoKroki.Name = "prawoKroki";
+            this.prawoKroki.Size = new System.Drawing.Size(75, 23);
+            this.prawoKroki.TabIndex = 6;
+            this.prawoKroki.Text = "prawo s1";
+            this.prawoKroki.UseVisualStyleBackColor = true;
+            this.prawoKroki.Click += new System.EventHandler(this.prawoKroki_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 174);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(334, 205);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(27, 124);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Connect";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "lewo s2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(506, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "prawo s2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(425, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "lewo s2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(506, 44);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "prawo s2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 455);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.prawoKroki);
+            this.Controls.Add(this.lewoKroki);
+            this.Controls.Add(this.speed);
+            this.Controls.Add(this.kat);
+            this.Controls.Add(this.kroki);
+            this.Controls.Add(this.prawoKat);
+            this.Controls.Add(this.lewoKat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button lewoKat;
+        private System.Windows.Forms.Button prawoKat;
+        private System.Windows.Forms.TextBox kroki;
+        private System.Windows.Forms.TextBox kat;
+        private System.Windows.Forms.TextBox speed;
+        private System.Windows.Forms.Button lewoKroki;
+        private System.Windows.Forms.Button prawoKroki;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Silnik_krokowy/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Threading;
+
+using FTD2XX_NET;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        FTDI myFtdiDevice;
+        byte[] tab = { 0x05, 0x01, 0x09, 0x08, 0x0A, 0x02, 0x06, 0x04 };
+        byte[] tab2 = { 0x05F, 0x01F, 0x09F, 0x08F, 0x0AF, 0x02F, 0x06F, 0x04F };
+
+
+        int index = 0;
+        UInt32 numBytesWritten = 0;
+
+        public void krok( int count, int direction, int speed, byte [] tab )
+        {
+            for ( int i = 0; i < count; i++ )
+            {
+                index += direction;
+                if ( index > 7 )
+                {
+                    index = 0;
+                }
+                else if ( index < 0 )
+                {
+                    index = 7;
+                }
+
+                byte[] x = { tab[index] };
+                    
+                 myFtdiDevice.Write( x, 1, ref numBytesWritten );
+                 Thread.Sleep( speed );
+                    
+                
+                
+                
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button5_Click( object sender, EventArgs e )
+        {
+            UInt32 ftdiDeviceCount = 0;
+            
+
+            // Create new instance of the FTDI device class
+            myFtdiDevice = new FTDI();
+            myFtdiDevice.GetNumberOfDevices( ref ftdiDeviceCount );
+            FTDI.FT_DEVICE_INFO_NODE[] ftdiDeviceList = new FTDI.FT_DEVICE_INFO_NODE[ftdiDeviceCount];
+            myFtdiDevice.GetDeviceList( ftdiDeviceList );
+            myFtdiDevice.OpenBySerialNumber( ftdiDeviceList[0].SerialNumber );
+            myFtdiDevice.SetBaudRate( 9600 );
+            myFtdiDevice.SetDataCharacteristics( FTDI.FT_DATA_BITS.FT_BITS_8, FTDI.FT_STOP_BITS.FT_STOP_BITS_1, FTDI.FT_PARITY.FT_PARITY_NONE );
+            myFtdiDevice.SetFlowControl( FTDI.FT_FLOW_CONTROL.FT_FLOW_RTS_CTS, 0x11, 0x13 );
+            myFtdiDevice.SetTimeouts( 5000, 0 );
+            richTextBox1.AppendText("polaczono");
+        }
+
+        private void lewoKroki_Click( object sender, EventArgs e )
+        {
+            int c = Convert.ToInt32( kroki.Text );
+            int s = Convert.ToInt32( speed.Text );
+            krok(c,-1,s, tab);
+        }
+
+        private void prawoKroki_Click( object sender, EventArgs e )
+        {
+            int c = Convert.ToInt32( kroki.Text );
+            int s = Convert.ToInt32( speed.Text );
+            krok( c, 1, s, tab );
+        }
+
+        private void lewoKat_Click( object sender, EventArgs e )
+        {
+            int c = 2 * (int)(Convert.ToInt32( kat.Text ) / 7.5);
+            int s = Convert.ToInt32( speed.Text );
+            krok( c, -1, s, tab );
+        }
+
+        private void prawoKat_Click( object sender, EventArgs e )
+        {
+            int c = 2 * (int)(Convert.ToInt32( kat.Text ) / 7.5);
+            int s = Convert.ToInt32( speed.Text );
+            krok( c, 1, s, tab );
+        }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+            int c = Convert.ToInt32( kroki.Text );
+            int s = Convert.ToInt32( speed.Text );
+            krok( c, -1, s, tab2 );
+        }
+
+        private void button2_Click( object sender, EventArgs e )
+        {
+            int c = Convert.ToInt32( kroki.Text );
+            int s = Convert.ToInt32( speed.Text );
+            krok( c, 1, s, tab2 );
+        }
+
+        private void button3_Click( object sender, EventArgs e )
+        {
+            int c = 2 * (int)(Convert.ToInt32( kat.Text ) / 0.75);
+            int s = Convert.ToInt32( speed.Text );
+            krok( c, -1, s, tab2 );
+        }
+
+        private void button4_Click( object sender, EventArgs e )
+        {
+            int c = 2 * (int)(Convert.ToInt32( kat.Text ) / 0.75);
+            int s = Convert.ToInt32( speed.Text );
+            krok( c, -1, s, tab2 );
+        }
+    }
+}
