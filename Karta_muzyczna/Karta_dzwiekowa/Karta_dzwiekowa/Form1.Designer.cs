f54d0455bd91cc6f6f79cd7303850a46ba096263
Unknown <pako-96@o2.pl>
2018-01-08 17:20:02 +0100

    Karta muzyczna działa

---
+++ Karta_muzyczna/Karta_dzwiekowa/Karta_dzwiekowa/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Karta_dzwiekowa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFile = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.labelFilePath = new System.Windows.Forms.Label();
+            this.listBoxFileInfo = new System.Windows.Forms.ListBox();
+            this.buttonRecord = new System.Windows.Forms.Button();
+            this.buttonFindDevice = new System.Windows.Forms.Button();
+            this.listBoxDevices = new System.Windows.Forms.ListBox();
+            this.labelRecording = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonFile
+            // 
+            this.buttonFile.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonFile.Location = new System.Drawing.Point(0, 37);
+            this.buttonFile.Name = "buttonFile";
+            this.buttonFile.Size = new System.Drawing.Size(140, 35);
+            this.buttonFile.TabIndex = 0;
+            this.buttonFile.Text = "Wybierz plik";
+            this.buttonFile.UseVisualStyleBackColor = true;
+            this.buttonFile.Click += new System.EventHandler(this.buttonFile_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonPlay.Location = new System.Drawing.Point(0, 93);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(140, 35);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "PLAY";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // labelFilePath
+            // 
+            this.labelFilePath.AutoSize = true;
+            this.labelFilePath.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelFilePath.Location = new System.Drawing.Point(-3, 148);
+            this.labelFilePath.Name = "labelFilePath";
+            this.labelFilePath.Size = new System.Drawing.Size(79, 15);
+            this.labelFilePath.TabIndex = 2;
+            this.labelFilePath.Text = "Wybrany plik:";
+            // 
+            // listBoxFileInfo
+            // 
+            this.listBoxFileInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxFileInfo.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBoxFileInfo.FormattingEnabled = true;
+            this.listBoxFileInfo.ItemHeight = 15;
+            this.listBoxFileInfo.Location = new System.Drawing.Point(190, 37);
+            this.listBoxFileInfo.Name = "listBoxFileInfo";
+            this.listBoxFileInfo.Size = new System.Drawing.Size(177, 94);
+            this.listBoxFileInfo.TabIndex = 3;
+            // 
+            // buttonRecord
+            // 
+            this.buttonRecord.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonRecord.Location = new System.Drawing.Point(185, 91);
+            this.buttonRecord.Name = "buttonRecord";
+            this.buttonRecord.Size = new System.Drawing.Size(177, 35);
+            this.buttonRecord.TabIndex = 4;
+            this.buttonRecord.Text = "Nagraj";
+            this.buttonRecord.UseVisualStyleBackColor = true;
+            this.buttonRecord.Click += new System.EventHandler(this.buttonRecord_Click);
+            // 
+            // buttonFindDevice
+            // 
+            this.buttonFindDevice.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonFindDevice.Location = new System.Drawing.Point(10, 31);
+            this.buttonFindDevice.Name = "buttonFindDevice";
+            this.buttonFindDevice.Size = new System.Drawing.Size(130, 35);
+            this.buttonFindDevice.TabIndex = 6;
+            this.buttonFindDevice.Text = "Wykryj mikrofon";
+            this.buttonFindDevice.UseVisualStyleBackColor = true;
+            this.buttonFindDevice.Click += new System.EventHandler(this.buttonFindDevice_Click);
+            // 
+            // listBoxDevices
+            // 
+            this.listBoxDevices.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBoxDevices.FormattingEnabled = true;
+            this.listBoxDevices.ItemHeight = 15;
+            this.listBoxDevices.Location = new System.Drawing.Point(190, 32);
+            this.listBoxDevices.Name = "listBoxDevices";
+            this.listBoxDevices.Size = new System.Drawing.Size(172, 34);
+            this.listBoxDevices.TabIndex = 7;
+            // 
+            // labelRecording
+            // 
+            this.labelRecording.AutoSize = true;
+            this.labelRecording.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelRecording.Location = new System.Drawing.Point(217, 24);
+            this.labelRecording.Name = "labelRecording";
+            this.labelRecording.Size = new System.Drawing.Size(0, 15);
+            this.labelRecording.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.labelRecording);
+            this.groupBox1.Controls.Add(this.listBoxFileInfo);
+            this.groupBox1.Controls.Add(this.labelFilePath);
+            this.groupBox1.Controls.Add(this.buttonPlay);
+            this.groupBox1.Controls.Add(this.buttonFile);
+            this.groupBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(386, 177);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informacje o pliku Wave";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 35);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Wybierz plik";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.listBoxDevices);
+            this.groupBox2.Controls.Add(this.buttonFindDevice);
+            this.groupBox2.Controls.Add(this.buttonRecord);
+            this.groupBox2.Location = new System.Drawing.Point(12, 215);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(386, 150);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Nagrywanie";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Wybierz mikrofon";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 372);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Obsługa karty dźwiękowej";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFile;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Label labelFilePath;
+        private System.Windows.Forms.ListBox listBoxFileInfo;
+        private System.Windows.Forms.Button buttonRecord;
+        private System.Windows.Forms.Button buttonFindDevice;
+        private System.Windows.Forms.ListBox listBoxDevices;
+        private System.Windows.Forms.Label labelRecording;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Karta_muzyczna/Karta_dzwiekowa/Karta_dzwiekowa/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Media;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Karta_dzwiekowa
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "";
+        // private SoundPlayer player;
+        private bool wasPlayed, wasRecored = false;
+        private string fileRecordPath = "";
+
+
+        NAudio.Wave.WaveIn sourceStream = null;
+        NAudio.Wave.DirectSoundOut waveOut = null;
+        NAudio.Wave.WaveFileWriter waveWriter = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFile_Click(object sender, EventArgs e)   // Wybranie pliku do odtworzenia
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Audio files (.wav)|*.wav";     // Akceptowalne formaty plików to WAV
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = fileDialog.FileName;
+                labelFilePath.Text = $"Wybrany plik: {filePath}";
+                FillListBox();
+            }
+        }
+
+        private void FillListBox()  // funkcja zczytująca nagłowek pliku WAV
+        {
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+                BinaryReader reader = new BinaryReader(fileStream);
+
+                byte[] wave = reader.ReadBytes(24);
+
+                fileStream.Position = 0;
+
+                int chunkID = reader.ReadInt32();
+                int fileSize = reader.ReadInt32();
+               // int riffType = reader.ReadInt32();
+
+                var fileFormat = Encoding.Default.GetString(wave);
+                string format = fileFormat.Substring(8, 4);
+                string subchunk1ID = fileFormat.Substring(12, 8);
+                int subchunk1Size = reader.ReadInt32(); 
+
+
+                reader.Close();
+
+                string chunkIDStr = $"Chunk ID: {chunkID}";
+                string fileSizeStr = $"Chunk size: {fileSize}";
+               
+                string fileFormatStr = $"Format: {format}";
+                string subchunk1IDStr = $"Subchunk ID: {subchunk1ID}";
+                string subchunk1SizeStr = $"Subchunk Size ID: {subchunk1Size}";
+              
+
+
+                listBoxFileInfo.Items.Clear();
+                listBoxFileInfo.Items.AddRange(new string[]
+                {
+                    "\tNagłówek pliku:",chunkIDStr, fileSizeStr, fileFormatStr,"\tOpis struktury audio:",subchunk1IDStr
+                   ,subchunk1SizeStr
+                });
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+
+
+            if (filePath == String.Empty)
+                MessageBox.Show("Wybierz plik!");
+            else
+            {
+                SoundPlayer simpleSound = new SoundPlayer(@filePath);
+                if (!wasPlayed)
+                {
+                    buttonPlay.Text = "STOP";
+                    wasPlayed = !wasPlayed;
+                    simpleSound.Play();
+                }
+                else
+                {
+                    buttonPlay.Text = "PLAY";
+                    wasPlayed = !wasPlayed;
+                    simpleSound.Stop();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+        private void buttonFindDevice_Click(object sender, EventArgs e)
+        {
+
+            label1.Visible = true;
+            List<NAudio.Wave.WaveInCapabilities> sources = new List<NAudio.Wave.WaveInCapabilities>();
+
+            for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+                sources.Add(NAudio.Wave.WaveIn.GetCapabilities(i));
+
+            listBoxDevices.Items.Clear();
+
+            int counter = 0;
+            foreach (var source in sources)
+            {
+                string item = source.ProductName;
+                listBoxDevices.Items.Add("Mikrofon "+ counter + "->" + item);
+                counter++;
+            }
+        }
+
+        private void sourceStream_DataAvailable(object sender, NAudio.Wave.WaveInEventArgs e)
+        {
+            if (waveWriter == null) return;
+
+            waveWriter.Write(e.Buffer, 0, e.BytesRecorded);
+            waveWriter.Flush();
+        }
+
+        private void buttonRecord_Click(object sender, EventArgs e)
+        {
+            if (wasRecored == false)
+            {
+                if (listBoxDevices.SelectedItems.Count == 0) return;
+               
+
+                if (fileRecordPath == "")
+                {
+                    MessageBox.Show("Wybierz miejsce w którym chcesz zapisać plik!");
+                }
+                else
+                {
+
+
+                    int deviceNumber = listBoxDevices.SelectedIndex;
+
+                    sourceStream = new NAudio.Wave.WaveIn();
+                    sourceStream.DeviceNumber = deviceNumber;
+                    sourceStream.WaveFormat = new NAudio.Wave.WaveFormat(44100, NAudio.Wave.WaveIn.GetCapabilities(deviceNumber).Channels);
+
+                    sourceStream.DataAvailable += new EventHandler<NAudio.Wave.WaveInEventArgs>(sourceStream_DataAvailable);
+                    waveWriter = new NAudio.Wave.WaveFileWriter(fileRecordPath, sourceStream.WaveFormat);
+
+                    sourceStream.StartRecording();
+                    
+                    buttonRecord.Text = "Nagrywanie...";
+                    wasRecored = true;
+                }
+            }
+            else if (wasRecored == true)
+            {
+                if (waveOut != null)
+                {
+                    waveOut.Stop();
+                    waveOut.Dispose();
+                    waveOut = null;
+                    buttonRecord.Text = "Nagraj";
+                }
+                if (sourceStream != null)
+                {
+                    sourceStream.StopRecording();
+                    sourceStream.Dispose();
+                    sourceStream = null;
+                    buttonRecord.Text = "Nagraj";
+                }
+                if (waveWriter != null)
+                {
+                    waveWriter.Dispose();
+                    waveWriter = null;
+                    buttonRecord.Text = "Nagraj";
+                }
+
+                labelRecording.Text = "";
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Wave File (*.wav)|*.wav;";
+            if (save.ShowDialog() != DialogResult.OK) return ;
+            else
+                fileRecordPath = save.FileName;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
